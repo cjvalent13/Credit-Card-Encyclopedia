4d9cd4963ef4badf089c9eb9c1eaa0ff1588c453
Magixion <63529626+Magixion@users.noreply.github.com>
2021-04-21 14:30:21 -0400

    Update PictureGrab.js

--- Homework2/html/PictureGrab.js
+++ Homework2/html/PictureGrab.js
@@ -1,5 +1,5 @@
 function GrabPicture()
 {
 	var AnimalName = document.getElementById('ani').value;
-	document.getElementById('picID').innerHTML = '<img src="./img/' +AnimalName+ '.jpg">';
+	document.getElementById('picID').innerHTML = '<img src="./img/' +AnimalName+ '.jpg" width="200" height="200">';
 }
